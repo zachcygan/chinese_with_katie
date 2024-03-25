0f86fefa4317404675288a4f183d1319baeed09d
zachcygan <zachcygan@gmail.com>
2024-03-24 19:42:24 -0700

    added dotted border

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -113,6 +113,8 @@ export default function Info() {
           </div>
         </div>
       </div>
+      <div className="border-b-[7px] border-blue-700 border-dotted p-10 mb-10 max-w-2xl mx-auto">
+      </div>
     </div>
   )
 }

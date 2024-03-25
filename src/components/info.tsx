0902806791cd03a224bd8c7e1a77150f618ebb5f
zachcygan <zachcygan@gmail.com>
2024-03-24 19:36:23 -0700

    added more info

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from "next/image";
 
 export default function Info() {
   return (
@@ -23,6 +24,93 @@ export default function Info() {
         </div>
         <div className="p-2">
           You'll be AMAZED at how much you can grow your skills!
+        </div>
+      </div>
+      <div className="text-center text-blue-900 text-2xl p-20">
+        <div>
+          My tutoring sessions are designed to meet the needs of English-speaking learners. I
+        </div>
+        <div>
+          guide beginning and intermediate Mandarin learners through language essentials to
+        </div>
+        <div>
+          gain a strong foundation.
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-between items-center mx-auto max-w-4xl p-2">
+          <div className="flex-1 flex justify-center">
+            <Image
+              src='/assets/images/info1.png'
+              width={250}
+              height={250}
+              alt='A+'
+            />
+          </div>
+          <div className="flex-1 flex flex-col justify-center items-start">
+            <div className="flex items-center justify-between">
+              <div className="py-2 text-3xl text-blue-900 font-semibold">Structured Lesson Plans</div>
+              {/* <svg preserveAspectRatio="xMidYMid meet" data-bbox="24.1 24.2 151.8 151.8" viewBox="24.1 24.2 151.8 151.8" xmlns="http://www.w3.org/2000/svg" data-type="color" role="presentation" aria-hidden="true" aria-label="" className="scale-[.3]">
+                <g>
+                  <path d="M100 176c-5.8 0-10.3-4.7-10.3-10.3 0-23.3-32-55.3-55.3-55.3-5.8 0-10.3-4.7-10.3-10.3 0-5.7 4.7-10.3 10.3-10.3 23.3 0 55.3-32 55.3-55.3 0-5.8 4.7-10.3 10.3-10.3 5.7 0 10.3 4.7 10.3 10.3 0 23.3 32 55.3 55.3 55.3 5.8 0 10.3 4.7 10.3 10.3 0 5.7-4.7 10.3-10.3 10.3-23.3 0-55.3 32-55.3 55.3 0 5.6-4.5 10.3-10.3 10.3zM66.4 99.9c13.2 8.2 25.5 20.3 33.6 33.6 8.2-13.2 20.3-25.5 33.6-33.6-13.2-8.2-25.5-20.3-33.6-33.6-8.2 13.3-20.3 25.4-33.6 33.6z" fill="#ffb600" data-color="1"></path>
+                </g>
+              </svg> */}
+            </div>
+            <div>
+              In my lessons, students can expect to learn with a lesson plan suited for their language level. To supplement, we'll additionally use workbooks and flashcards. Each student will have access to the lesson plan and notes used in our sessions and parents will receive personalized progress reports
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center mx-auto max-w-4xl p-2">
+          <div className="flex-1 flex flex-col justify-center items-start">
+            <div className="flex items-center justify-between">
+              <div className="py-2 text-3xl text-blue-900 font-semibold">Flexability</div>
+              {/* <svg preserveAspectRatio="xMidYMid meet" data-bbox="24.1 24.2 151.8 151.8" viewBox="24.1 24.2 151.8 151.8" xmlns="http://www.w3.org/2000/svg" data-type="color" role="presentation" aria-hidden="true" aria-label="" className="scale-[.3]">
+                <g>
+                  <path d="M100 176c-5.8 0-10.3-4.7-10.3-10.3 0-23.3-32-55.3-55.3-55.3-5.8 0-10.3-4.7-10.3-10.3 0-5.7 4.7-10.3 10.3-10.3 23.3 0 55.3-32 55.3-55.3 0-5.8 4.7-10.3 10.3-10.3 5.7 0 10.3 4.7 10.3 10.3 0 23.3 32 55.3 55.3 55.3 5.8 0 10.3 4.7 10.3 10.3 0 5.7-4.7 10.3-10.3 10.3-23.3 0-55.3 32-55.3 55.3 0 5.6-4.5 10.3-10.3 10.3zM66.4 99.9c13.2 8.2 25.5 20.3 33.6 33.6 8.2-13.2 20.3-25.5 33.6-33.6-13.2-8.2-25.5-20.3-33.6-33.6-8.2 13.3-20.3 25.4-33.6 33.6z" fill="#ffb600" data-color="1"></path>
+                </g>
+              </svg> */}
+            </div>
+            <div>
+              <div>
+                Being an online tutor makes it easy for me to be accommodating of your schedule! My hours for tutoring are  from Monday to Friday, 5:30pm -9pm MST .
+              </div>
+              <div className="p-1 italic font-semibold">
+                *Ensure to check availability and reserve your spot.
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 flex justify-center py-2">
+            <Image
+              src='/assets/images/info2.png'
+              width={250}
+              height={250}
+              alt='A+'
+            />
+          </div>
+        </div>
+        <div className="flex justify-between items-center mx-auto max-w-4xl p-2">
+          <div className="flex-1 flex justify-center">
+            <Image
+              src='/assets/images/info3.png'
+              width={250}
+              height={250}
+              alt='A+'
+            />
+          </div>
+          <div className="flex-1 flex flex-col justify-center items-start">
+            <div className="flex items-center justify-between">
+              <div className="py-2 text-3xl text-blue-900 font-semibold">Homework Help</div>
+              {/* <svg preserveAspectRatio="xMidYMid meet" data-bbox="24.1 24.2 151.8 151.8" viewBox="24.1 24.2 151.8 151.8" xmlns="http://www.w3.org/2000/svg" data-type="color" role="presentation" aria-hidden="true" aria-label="" className="scale-[.3]">
+                <g>
+                  <path d="M100 176c-5.8 0-10.3-4.7-10.3-10.3 0-23.3-32-55.3-55.3-55.3-5.8 0-10.3-4.7-10.3-10.3 0-5.7 4.7-10.3 10.3-10.3 23.3 0 55.3-32 55.3-55.3 0-5.8 4.7-10.3 10.3-10.3 5.7 0 10.3 4.7 10.3 10.3 0 23.3 32 55.3 55.3 55.3 5.8 0 10.3 4.7 10.3 10.3 0 5.7-4.7 10.3-10.3 10.3-23.3 0-55.3 32-55.3 55.3 0 5.6-4.5 10.3-10.3 10.3zM66.4 99.9c13.2 8.2 25.5 20.3 33.6 33.6 8.2-13.2 20.3-25.5 33.6-33.6-13.2-8.2-25.5-20.3-33.6-33.6-8.2 13.3-20.3 25.4-33.6 33.6z" fill="#ffb600" data-color="1"></path>
+                </g>
+              </svg> */}
+            </div>
+            <div>
+              Need help on your homework? Feel free to send this to me before our lesson! We'll work through your problems and learn what areas to focus on.reports
+            </div>
+          </div>
         </div>
       </div>
     </div>

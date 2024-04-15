384bae4d7408a897710be25d81b6b5207825b071
zachcygan <zachcygan@gmail.com>
2024-04-14 19:44:49 -0700

    test

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -19,12 +19,11 @@ export default function AboutMe() {
           <div className='text-5xl py-10'>
             About me
           </div>
-          <div className='py-5'>
+          <div className='py-5 '>
             <span className='text-2xl font-semibold'>大家好!</span> It's Katie — your guide to learning Mandarin!
           </div>
           <div className='py-5 max-w-lg pb-24 mx-auto'>
             Ever since I was a child, I have always been fascinated by learning foreign languages and cultures. With a new perspective gained after graduating college, I recognized a crucial demand for more engaging and effective approaches to learning Chinese. As a non-native speaker,  I've experienced the challenges of learning Chinese first hand, making me uniquely suited to understand the learning needs of English speakers. My passion lies in helping others and making education accessible. I'm excited to make your language learning journey a enriching and enjoyable experience!
-          </div>
         </div>
       </div>
       <div className=''>
